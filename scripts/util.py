5f65a858fe63c76254444845a7338cc1bdd49473
Lukas K <lu@0x83.eu>
2019-08-12 00:18:51 +0200

    add samsung CL capacitors

--- scripts/util.py
+++ scripts/util.py
@@ -19,3 +19,36 @@ class UUIDGenerator:
 				fi.write(key + "\t" + str(uu) + "\n")
 			self.uuids[key] = uu
 		return uu
+
+prefixes = {
+	-15 : "f",
+	-12 : "p",
+	 -9 : "n",
+	 -6 : "µ",
+	 -3 : "m",
+	  0 : "",
+	  3 : "k",
+	  6 : "M",
+	  9 : "G",
+	 12 : "T",
+}
+
+def format_si(value, digits, strip=True) :
+	v = abs(value)
+	exp = 0
+	while v >= 1e3 and exp <= 12 :
+		v /= 1e3
+		exp += 3
+	if v > 1e-15 :
+		while v < 1 and exp >= -12 :
+			v *= 1e3
+			exp -= 3
+	if value < 0 :
+		s = "-"
+	else :
+		s = ""
+	n = ("%." + str(digits) + "f")%v
+	if strip :
+		n = n.rstrip("0").rstrip(".")
+	s+= n + " " + prefixes[exp]
+	return s
