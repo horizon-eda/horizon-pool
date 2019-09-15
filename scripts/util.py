b41dd7b7eec5c3b2b3e4c53e04361ad901177c2d
David Lamparter <equinox@diac24.net>
2019-09-15 09:48:10 +0200

    scripts: reformat util.py to PEP-8 style
    Unlike e.g. C or C++, Python has a language-offical coding style.  Among
    other things, it's 4 spaces for indentation.
    
    The changes in this commit are relased into the public domain as in
    https://creativecommons.org/publicdomain/zero/1.0/deed (CC0).
    Note that this only applies to the changes, derivative copyright will
    still apply.

--- scripts/util.py
+++ scripts/util.py
@@ -6,23 +6,23 @@ self_path = os.path.dirname(os.path.abspath(__file__))
 pool_path = os.getenv("HORIZON_POOL") or os.path.dirname(self_path)
 
 class UUIDGenerator:
-	def __init__(self, filename) :
-		self.filename = filename
-		if os.path.isfile(filename) :
-			with open(filename, "r") as fi :
-				self.uuids = {k:uuid.UUID(v) for k,v in (l.strip().split("\t") for l in fi.readlines() if len (l.strip()))}
-		else :
-			self.uuids = {}
-		
-	def get(self, key) :
-		if key in self.uuids :
-			return self.uuids[key]
-		else :
-			uu = uuid.uuid4()
-			with open(self.filename, "a") as fi :
-				fi.write(key + "\t" + str(uu) + "\n")
-			self.uuids[key] = uu
-		return uu
+    def __init__(self, filename) :
+        self.filename = filename
+        if os.path.isfile(filename) :
+            with open(filename, "r") as fi :
+                self.uuids = {k:uuid.UUID(v) for k,v in (l.strip().split("\t") for l in fi.readlines() if len (l.strip()))}
+        else :
+            self.uuids = {}
+
+    def get(self, key) :
+        if key in self.uuids :
+            return self.uuids[key]
+        else :
+            uu = uuid.uuid4()
+            with open(self.filename, "a") as fi :
+                fi.write(key + "\t" + str(uu) + "\n")
+            self.uuids[key] = uu
+        return uu
 
 class CachedURL(object):
     '''
@@ -108,40 +108,40 @@ class CachedURL(object):
         return self._data
 
 prefixes = {
-	-15 : "f",
-	-12 : "p",
-	 -9 : "n",
-	 -6 : "µ",
-	 -3 : "m",
-	  0 : "",
-	  3 : "k",
-	  6 : "M",
-	  9 : "G",
-	 12 : "T",
+    -15 : "f",
+    -12 : "p",
+     -9 : "n",
+     -6 : "µ",
+     -3 : "m",
+      0 : "",
+      3 : "k",
+      6 : "M",
+      9 : "G",
+     12 : "T",
 }
 
 reversepfx = dict([(v, k) for k, v in prefixes.items()])
 reversepfx['u'] = -6
 
 def format_si(value, digits, strip=True) :
-	v = abs(value)
-	exp = 0
-	while v >= 1e3 and exp <= 12 :
-		v /= 1e3
-		exp += 3
-	if v > 1e-15 :
-		while v < 1 and exp >= -12 :
-			v *= 1e3
-			exp -= 3
-	if value < 0 :
-		s = "-"
-	else :
-		s = ""
-	n = ("%." + str(digits) + "f")%v
-	if strip :
-		n = n.rstrip("0").rstrip(".")
-	s+= n + " " + prefixes[exp]
-	return s
+    v = abs(value)
+    exp = 0
+    while v >= 1e3 and exp <= 12 :
+        v /= 1e3
+        exp += 3
+    if v > 1e-15 :
+        while v < 1 and exp >= -12 :
+            v *= 1e3
+            exp -= 3
+    if value < 0 :
+        s = "-"
+    else :
+        s = ""
+    n = ("%." + str(digits) + "f")%v
+    if strip :
+        n = n.rstrip("0").rstrip(".")
+    s+= n + " " + prefixes[exp]
+    return s
 
 def parse_si(value):
     value = value.strip()
